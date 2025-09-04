6a7f987ec50025e96ffeef58face0f64b69050d5
Tom Boone <tbc@tomboone.com>
2025-09-04 14:52:43 -0400

    add terraform to deploy workflow; add slot swap workflow

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "function_app_name" {
+  value = azurerm_linux_function_app.main.name
+}
+
+output "function_app_staging_slot_name" {
+  value = azurerm_linux_function_app_slot.stage.name
+}
+
+output "function_app_resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
+
+output "function_app_python_version" {
+  value = azurerm_linux_function_app.main.site_config.application_stack.python_version
+}
